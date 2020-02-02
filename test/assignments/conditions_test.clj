3f159bbf0d378dd9f94fd5388cd33c4b5083f9c1
Rahul Madaan <rahulma@thoughtworks.com>
2020-02-02 22:44:02 +0530

    Add test for informative-divide

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -11,12 +11,17 @@
 
 (deftest truthy-harishchandra
   (testing "truthy value (0)"
-    (is (= 0 (harishchandra 0)))
-    )
+    (is (= 0 (harishchandra 0))))
   (testing "truthy value (2)"
-    (is (= 2 (harishchandra 2)))
-    )
+    (is (= 2 (harishchandra 2))))
   (testing "falsy value nil"
-    (is (= nil (harishchandra nil)))
-    )
+    (is (= nil (harishchandra nil)))))
+
+(deftest informative-divide-test
+  (testing "return 2 by dividing 4 by 2"
+    (is (= 2 (informative-divide 4 2))))
+  (testing "return infinity by dividing 3 by 0"
+    (is (= :infinite (informative-divide 3 0))))
   )
+
+
