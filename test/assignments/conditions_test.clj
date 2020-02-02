405c0906eb041baf3ad79ead34e6627ee18e18ff
Rahul Madaan <rahulma@thoughtworks.com>
2020-02-02 22:32:39 +0530

    Add test for harishchandra

--- test/assignments/conditions_test.clj
+++ test/assignments/conditions_test.clj
@@ -7,3 +7,16 @@
     (is (= 2 (safe-divide 4 2))))
   (testing "zero denominator"
     (is (nil? (safe-divide 3 0)))))
+
+
+(deftest truthy-harishchandra
+  (testing "truthy value (0)"
+    (is (= 0 (harishchandra 0)))
+    )
+  (testing "truthy value (2)"
+    (is (= 2 (harishchandra 2)))
+    )
+  (testing "falsy value nil"
+    (is (= nil (harishchandra nil)))
+    )
+  )
